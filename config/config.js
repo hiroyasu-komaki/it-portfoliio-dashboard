334df6fb802d4ddfff526fde34089b6208c8ae8c
Hiroyasu Komaki <hiroyasukomaki@HiroyasunoMac-mini.local>
2025-11-15 16:29:04 +0900

    first commit

---
+++ config/config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                "kkc-orange": "#FF7600",
+                "kkc-orange-light": "#FF8F33",
+                "kkc-orange-dark": "#CC5F00"
+            }
+        } 
+    }
+};
